57880f07e32bea05573ac28a274fbc9c25c09af7
Ayush kumar <ayush.bcepatna@gmail.com>
2025-04-21 20:41:44 +0530

    I am a commit message

--- Sorting/selection_sort.c
+++ Sorting/selection_sort.c
@@ -1,27 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    
-    int arr[] = {12,1,3,12,42,13};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    printf("Size = %d\n",size);
+int main()
+{
 
-    for(int i = 0; i<size; i++){
+    int arr[] = {12, 1, 3, 12, 42, 13};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printf("Size = %d\n", size);
+
+    for (int i = 0; i < size; i++)
+    {
         int min_idx = i;
-        for(int j = i+1; j<size; j++){
-            if(arr[j]<arr[min_idx]){
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min_idx])
+            {
                 min_idx = j;
             }
         }
-        
+
         int temp = arr[min_idx];
         arr[min_idx] = arr[i];
         arr[i] = temp;
-    
     }
-    for(int i = 0; i<size; i++){
-        printf("%d\t",arr[i]);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\t", arr[i]);
     }
-
-    return 0;
 }
